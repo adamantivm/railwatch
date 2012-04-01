331c08c4f5066bb2ad1719d05e41f9b679cef147
Julian Cerruti <jcerruti@ar.ibm.com>
2012-04-01 15:47:45 -0300

    Diffing an image and displaying it

--- tests0/display.py
+++ tests0/display.py
@@ -1,8 +1,10 @@
 import cv, time
 
 win = "win-0"
-im = cv.LoadImageM("seq-1.jpg")
-print im
+im1 = cv.LoadImageM("seq-1.jpg")
+im2 = cv.LoadImageM("seq-2.jpg")
+diff = cv.CreateMat( im1.rows, im1.cols, im1.type)
+cv.AbsDiff( im1, im2, diff)
 cv.NamedWindow( win)
-cv.ShowImage( win, im)
+cv.ShowImage( win, diff)
 cv.WaitKey( 0)
