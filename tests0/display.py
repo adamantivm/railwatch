7deeeb40f3966b4edcac6e7a4bb58eb2b9bd7cf4
Julian Cerruti <jcerruti@ar.ibm.com>
2012-04-02 14:22:18 -0300

    Reorganizing files for use inside Eclipse

--- tests0/display.py
+++ tests0/display.py
@@ -1,12 +1,14 @@
-import cv, time
+import cv
 
-win = "win-0"
-cv.NamedWindow( win)
-im_prev = cv.LoadImageM("seq-1.jpg")
+WIN = "win-0"
+DATA_DIR = "../runtime"
+
+cv.NamedWindow( WIN)
+im_prev = cv.LoadImageM("%s/seq-1.jpg" % DATA_DIR)
 im_diff = cv.CreateMat( im_prev.rows, im_prev.cols, im_prev.type)
 for i in range(2,300):
-    im_cur = cv.LoadImageM("seq-%d.jpg" % i)
+    im_cur = cv.LoadImageM("%s/seq-%d.jpg" % (DATA_DIR,i))
     cv.AbsDiff( im_prev, im_cur, im_diff)
-    cv.ShowImage( win, im_diff)
+    cv.ShowImage( WIN, im_diff)
     cv.WaitKey( 200)
     im_prev = im_cur
