c21d4f1410c5a698c88237b8c477d129af3bca95
Julian Cerruti <jcerruti@ar.ibm.com>
2012-04-02 15:02:00 -0300

    New version which I don't like at all using optical flows

--- tests0/display.py
+++ tests0/display.py
@@ -1,14 +1,28 @@
 import cv
 
 WIN = "win-0"
+WIN_X = "win-x"
+WIN_Y = "win-y"
 DATA_DIR = "../runtime"
 
 cv.NamedWindow( WIN)
 im_prev = cv.LoadImageM("%s/seq-1.jpg" % DATA_DIR)
-im_diff = cv.CreateMat( im_prev.rows, im_prev.cols, im_prev.type)
+
+velx = cv.CreateMat( im_prev.rows, im_prev.cols, cv.CV_32FC1)
+vely = cv.CreateMat( im_prev.rows, im_prev.cols, cv.CV_32FC1)
+
+im_prev_8 = cv.CreateMat( im_prev.rows, im_prev.cols, cv.CV_8UC1)
+im_curr_8 = cv.CreateMat( im_prev.rows, im_prev.cols, cv.CV_8UC1)
+
 for i in range(2,300):
-    im_cur = cv.LoadImageM("%s/seq-%d.jpg" % (DATA_DIR,i))
-    cv.AbsDiff( im_prev, im_cur, im_diff)
-    cv.ShowImage( WIN, im_diff)
+    im_curr = cv.LoadImageM("%s/seq-%d.jpg" % (DATA_DIR,i))
+
+    cv.CvtColor( im_prev, im_prev_8, cv.CV_BGR2GRAY)
+    cv.CvtColor( im_curr, im_curr_8, cv.CV_BGR2GRAY)
+    cv.CalcOpticalFlowHS( im_prev_8, im_curr_8, 0, velx, vely, 0.0001, (cv.CV_TERMCRIT_ITER, 10, 0))
+
+    cv.ShowImage( WIN_X, velx)
+    cv.ShowImage( WIN_Y, vely)
+    cv.ShowImage( WIN, im_curr_8)
     cv.WaitKey( 200)
-    im_prev = im_cur
+    im_prev = im_curr
